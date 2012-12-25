d34848963cb4f0717abbbb08bc0f71d375a3e92f
Martin Lenders <mail@martin-lenders.de>
2012-12-25 21:06:46 +0100

    Add abstract network connection

--- wakefs/conn.py
+++ wakefs/conn.py
@@ -37,3 +37,35 @@ class LocalConnection(Connection):
     def update(self, model_object):
         # TODO
         print 'Update:', repr(model_object)
+
+class NetConnection(Connection):
+    def __init__(self, host, mac, port, path):
+        super(NetConnection, self).__init__()
+        if len(mac) == 12 + 5:
+            sep = mac[2]
+            mac = mac.replace(sep, '')
+        if len(mac) == 12:
+            self.mac = mac
+        else:
+            raise ValueError('Incorrect MAC address format')
+        self.host = host
+        self.mac = mac
+        self.port = port
+        self.path = path
+
+    def wake_host(self):
+        # Source: http://code.activestate.com/recipes/358449-wake-on-lan/
+        data = ''.join(['FFFFFFFFFFFF', macaddress * 20]) # pad sync stream
+        send_data = ''
+        # Split up the hex values and pack.
+        for i in range(0, len(data), 2):
+            send_data = ''.join([send_data,
+                                 struct.pack('B', int(data[i: i + 2], 16))])
+
+        # Broadcast it to the LAN.
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.sendto(send_data, ('<broadcast>', 7))
+
+    def connect(self):
+        raise NotImplemented
