d2e31150d147c43ad35d8358ade2086baa0646b2
Martin Lenders <mail@martin-lenders.de>
2012-12-25 18:26:31 +0100

    Add stats type

--- wakefs/model.py
+++ wakefs/model.py
@@ -5,6 +5,32 @@ import wakefs.db
 import wakefs.utils
 import wakefs.conn
 
+class Stats(object):
+    valid_attributes = {
+            'st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid',
+            'st_gid', 'st_rdev', 'st_size', 'st_blksize', 'st_blocks',
+            'st_atime', 'st_mtime', 'st_ctime'
+        }
+
+    def __init__(self, **stat):
+        if set(stat.keys()) == Stats.valid_attributes:
+            self.stat = stat
+        else:
+            raise ValueError("Parameters %s wrong" %
+                    str(tuple(Stats.valid_attributes ^ set(stat.keys()))))
+
+    def __getattribute__(self, name):
+        if name in Stats.valid_attributes:
+            return self.stat[name]
+        else:
+            return object.__getattribute__(self,name)
+
+    def __repr__(self):
+        repr = "Stats("
+        for a in Stats.valid_attributes:
+            repr += a + '=' + str(self.stat[a]) + ','
+        return repr[:-1] + ")"
+
 class File(object):
     def __init__(self, name, location=None):
         wakefs.db.initialise()
@@ -50,6 +76,24 @@ class File(object):
     @property
     def name(self):
         return self.__get_db_col('name')
+
+    @property
+    def stat(self):
+        return Stats(
+                self.__get_db_col('st_mode'),
+                self.__get_db_col('st_ino'),
+                self.__get_db_col('st_dev'),
+                self.__get_db_col('st_nlink'),
+                self.__get_db_col('st_uid'),
+                self.__get_db_col('st_gid'),
+                self.__get_db_col('st_rdev'),
+                self.__get_db_col('st_size'),
+                self.__get_db_col('st_blksize'),
+                self.__get_db_col('st_blocks'),
+                self.__get_db_col('st_atime'),
+                self.__get_db_col('st_mtime'),
+                self.__get_db_col('st_ctime'),
+            )
 
     def __unicode__(self):
         return self.name
